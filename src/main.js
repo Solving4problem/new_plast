a55adbf78657aa14cb9331320b2d35f00439a92d
Solving4problem <solving4problem@gmail.com>
2019-10-22 22:52:31 +0300

    First commit with clone data

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './donations.scss';
+import './support-banner.scss';
+import './attraction.scss';
+import './donations_counter.scss';
+import './project-list.scss';
+import './about.scss';
+import './supporters.scss';
+import './contacts.scss';
+import './account_payment.scss';
+import './footer.scss';
+import './main.scss';
+import './header.scss';
